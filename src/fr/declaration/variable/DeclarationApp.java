f3f0c8b33fed7d7f36424e0bd86eb075029806bc
Pierre MABUSO <pierremabuso@gmail.com>
2025-03-21 11:36:30 +0100

    feat: tp3 v1

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,18 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+    public static void main(String[] args) {
+
+        byte a = 13;
+        short b = 28026;
+        int c = 123456;
+        long d = 20000L;
+        float e = 3.8f;
+        double f = 1.8d;
+        System.out.println(a);
+
+        String randomString = "Voici le résultat d'un calcul : 1 + 5 = 6";
+        System.out.println(randomString);
+    }
+}
